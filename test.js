afb7fe1a5e81481c938655aa3c061e8119c8927e
Kelvin Ndungu <wanjakelvinndungu@gmail.com>
2023-09-23 22:57:22 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,1 @@
-var1 config = {
-    bucket: "examplebucket1111",
-    awsKey: "AKIAIOSFODNN7EXAMPLE111",
-    awsSecret: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-    server: "someserver.example.com"
-};
+var1 
